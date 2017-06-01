553fa76a79bdd267a86640c364802332d33b9c64
Agnaldo Samuel <agnaldosamuel3@gmail.com>
2017-06-01 16:38:12 +0200

    call mysql via bash to import db

--- filemanager.py
+++ filemanager.py
@@ -5,6 +5,20 @@ import shutil
 import sys
 
 
+def matchfacilityname(arr_facilities,f_name):
+    facility = ''
+    for f in arr_facilities:
+        # Alguns nomes nao serao encontrados na lista (config.yaml)
+        # casos em que se escreve mal o nome da US no script de backup
+        # nestes casos deve-se corrigir o nome no script de backup (ex: Malhagalene -> Malhangalene)
+        if f.lower() in f_name.lower():
+            facility = f
+            break
+        else:
+            facility = f_name.lower()[:len(f_name) - 4]  # No match found, remove .sql extension
+    return facility
+
+
 def decompressfile(filename, outdir, destdir):
     if tarfile.is_tarfile(filename):
         try:
@@ -29,6 +43,18 @@ def decompressfile(filename, outdir, destdir):
     else:
         logging.debug(filename + " is not an .tar file")
         print(filename + " is not an .tar file")
+
+    return
+
+
+def renamebackupfiles(vector_facilities,sql_files_dir):
+    if os.path.isdir(sql_files_dir):
+        os.chdir(sql_files_dir)
+        for root, dirs, files in os.walk('.', topdown=True):
+            for f in files:
+                if f.endswith(".sql"):
+                    new_name = matchfacilityname(vector_facilities,f)
+                    os.rename(f, 'cs_' + new_name + '.sql')
 
     return
 
--- taskexecutor.py
+++ taskexecutor.py
@@ -1,9 +1,9 @@
 import os
-from filemanager import decompressfile
+import filemanager
 import logging
 import yaml
-# import pymysql # uses a c extension to connect to mysql
-import MySQLdb  # pure python library
+import dbacess
+
 
 # Read config file
 with open("/home/agnaldo/config.yaml", 'r') as stream:
@@ -16,44 +16,9 @@ with open("/home/agnaldo/config.yaml", 'r') as stream:
         basedir = config['basedir']
         outdir = basedir + config['outdir']
         backup_dir = basedir + config['backup_dir']
-        datadir = basedir + config['datadir']
-
-
-# Misc Functions
-def mysqlgetconnection(usr, pwd):
-    # Connect to the database
-    conn = MySQLdb.connect(passwd=pwd, db=usr)
-    return conn
-
-
-def renamebackupfiles(sql_files_dir):
-    if os.path.isdir(sql_files_dir):
-        os.chdir(sql_files_dir)
-        for root, dirs, files in os.walk('.', topdown=True):
-            for f in files:
-                if f.endswith(".sql"):
-                    new_name = matchfacilityname(f)
-                    # Alguns nomes nao serao encontrados na lista
-                    # casos em que se escreve mal o nome da US no script de backup
-                    # nestes casos deve-se corrigir o nome no script de backup (ex: Malhagalene -> Malhangalene)
-                    if new_name != '':
-                        os.rename(f, 'cs_' + new_name + '.sql')
-                    else:
-                        print(
-                            f + " Nao iguala-se com algum nome na lista das US. Verifique se o nome do ficheiro esta correcto")
-                        logging.debug(
-                            f + " Nao iguala-se com algum nome na lista das US. Verifique se o nome do ficheiro esta correcto")
-    return
-
-
-def matchfacilityname(f_name):
-    arr_facilities = config['facilities']
-    facility = ''
-    for f in arr_facilities:
-        if f.lower() in f_name.lower():
-            facility = f
-            break
-    return facility
+        datadir = config['datadir']
+        mysql_username = config['mysql_username']
+        mysql_password = config['mysql_password']
 
 
 if os.path.isdir(basedir):
@@ -75,12 +40,14 @@ if os.path.isdir(basedir):
                     numtarfiles += 1
                     logging.info("File found: " + name)
                     print("File found: " + name)
-                    decompressfile(name, outdir, datadir)
+                    filemanager.decompressfile(name, outdir, datadir)
         if numtarfiles == 0:
             logging.info("No backup file was found:")
         else:
             print("Renaming sql_files in " + datadir)
-            renamebackupfiles(datadir)
+            facilities = config['facilities']
+            filemanager.renamebackupfiles(facilities,datadir)
+            dbacess.beginmysqljob(numtarfiles, datadir)
 
     else:
         logging.debug("IOerror: path " + backup_dir + " was not found")
@@ -89,30 +56,3 @@ else:
     logging.debug("IOerror: path " + basedir + " was not found")
     print("IOerror: path " + basedir + " was not found")
 
-
-def beginmysqljob(extracted_files, sql_files_dir):
-    if os.path.isdir(sql_files_dir):
-        os.chdir(sql_files_dir)
-        sqlfiles = 0
-
-        for path, dirs, files in os.walk('.', topdown=True):
-            for n in files:
-                if n.endswith(".sql"):
-                    sqlfiles += 1
-        if sqlfiles == 0:
-            logging.info("No backup file extracted. check if you have backup files in the backup folder ")
-            print("No backup file extracted. check if you have backup files in the backup folder ")
-        elif sqlfiles == extracted_files and sqlfiles > 0:
-            logging.info("Execution ok, starting mysql jobs...")
-            print("Execution ok, starting mysql jobs...")
-            # TODO - start mysql jobs here
-
-        elif sqlfiles != extracted_files:
-            logging.info("Some backup files (tar) were not extracted, check the logs for more details")
-            print("Info: Some backup files (tar) were not extracted, check the logs for more details")
-            print("starting mysql jobs...")
-            # TODO - start mysql jobs here
-
-    else:
-        logging.debug("IOerror: path " + sql_files_dir + " was not found")
-        print("IOerror: path " + sql_files_dir + " was not found")
